27deab5156d47f745871521468c4fc0a83749fb5
Davi Reis <daviavr@gmail.com>
2024-10-22 09:32:04 -0300

    minor corrections

--- src/components/register/register.test.tsx
+++ src/components/register/register.test.tsx
@@ -1,8 +1,21 @@
 import { render, screen } from "@testing-library/react";
 import Register from "./Register";
+const API_URL = "http://localhost:3000/mock";
 
-describe("Component Register", () => {
-  const API_URL = "http://localhost:3000/mock";
+describe("Componente Register", () => {
+  const mockAuth = (value) => value
+  it("Register tem que ser renderizado corretamente", () => {
+    render(<Register
+      API_URL={API_URL}
+      setIsAuthenticated={mockAuth}
+      expirationDelay={200}
+    />);
+    const element = screen.queryByText("Registro");
+    expect(element).toBeInTheDocument();
+  });
+});
+
+describe("", () => {
   const mockAuth = (value) => value
   it("Register tem que ser renderizado corretamente", () => {
     render(<Register
